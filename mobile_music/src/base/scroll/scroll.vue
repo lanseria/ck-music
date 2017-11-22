a85dee6f05721737f34afb210b3179cea1a41659
lanseria <zhangchao564265135@hotmail.com>
2017-11-22 14:25:58 +0800

    delete some unused

--- mobile_music/src/base/scroll/scroll.vue
+++ mobile_music/src/base/scroll/scroll.vue
@@ -78,7 +78,6 @@ export default {
     },
     refresh () {
       this.scroll && this.scroll.refresh()
-      // console.log(this.scroll.maxScrollY)
     },
     scrollTo () {
       this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
@@ -96,6 +95,3 @@ export default {
   }
 }
 </script>
-<style lang="stylus" scoped>
-
-</style>
