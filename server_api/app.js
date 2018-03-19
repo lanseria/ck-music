e4e1cd25568a8f5676d56b92362935a8874d017d
lanseria <zhangchao564265135@hotmail.com>
2018-03-19 22:08:31 +0800

    remove yarn.lock to avoid ci test Error

--- server_api/app.js
+++ server_api/app.js
@@ -1,28 +1,12 @@
-var got = require('got')
-var express = require('express')
-
-var port = 7992
-
-var app = express()
-var apiRouters = express.Router()
-
-// apiRouters.get('/musicUrl', function (req, res) {
-//   var url = `http://isure.stream.qqmusic.qq.com/C100${req.query.songmid}.m4a?fromtag=32`
-//   got.get(url, {
-//     headers: {
-//       referer: 'https://u.y.qq.com',
-//       host: 'u.y.qq.com'
-//     }
-//   }).then((response) => {
-//     res.send(response.body)
-//   }).catch(e => {
-//     console.log(e)
-//   })
-// })
+const got = require('got')
+const express = require('express')
+const config = require('./config')
+const { port, urls } = config
+const app = express()
+const apiRouters = express.Router()
 
 apiRouters.get('/getPlaylist', function (req, res) {
-  var url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
-  got.get(url, {
+  got.get(urls.GETPLAYLIST, {
     headers: {
       referer: 'https://u.y.qq.com',
       host: 'u.y.qq.com'
@@ -36,8 +20,7 @@ apiRouters.get('/getPlaylist', function (req, res) {
 })
 
 apiRouters.get('/getDiscList', function (req, res) {
-  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
-  got.get(url, {
+  got.get(urls.GETDISCLIST, {
     headers: {
       referer: 'https://c.y.qq.com/',
       host: 'c.y.qq.com'
@@ -50,8 +33,7 @@ apiRouters.get('/getDiscList', function (req, res) {
   })
 })
 apiRouters.get('/getDiscSongs', function (req, res) {
-  var url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
-  got.get(url, {
+  got.get(urls.GETDISCSONGS, {
     headers: {
       referer: 'https://c.y.qq.com/',
       host: 'c.y.qq.com'
@@ -72,8 +54,7 @@ apiRouters.get('/getDiscSongs', function (req, res) {
   })
 })
 apiRouters.get('/getTopList', function (req, res) {
-  var url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
-  got.get(url, {
+  got.get(urls.GETTOPLIST, {
     headers: {
       referer: 'https://c.y.qq.com/',
       host: 'c.y.qq.com'
@@ -86,8 +67,7 @@ apiRouters.get('/getTopList', function (req, res) {
   })
 })
 apiRouters.get('/search', function (req, res) {
-  var url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
-  got.get(url, {
+  got.get(urls.SEARCH, {
     headers: {
       referer: 'https://c.y.qq.com/',
       host: 'c.y.qq.com'
@@ -100,15 +80,13 @@ apiRouters.get('/search', function (req, res) {
   })
 })
 apiRouters.get('/lyric', function (req, res) {
-  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
-  got.get(url, {
+  got.get(urls.LYRIC, {
     headers: {
       referer: 'https://c.y.qq.com/',
       host: 'c.y.qq.com'
     },
     query: req.query
   }).then((response) => {
-    // res.send(response.body)
     var ret = response.body
     if (typeof ret === 'string') {
       var reg = /^\w+\(({[^]+})\)$/
---
+++ server_api/config.js
@@ -0,0 +1,11 @@
+const config = {
+  port: 7992,
+  urls: {
+    GETPLAYLIST: 'https://u.y.qq.com/cgi-bin/musicu.fcg',
+    GETDISCLIST: 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg',
+    GETDISCSONGS: 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg',
+    GETTOPLIST: 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg',
+    SEARCH: 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp',
+    LYRIC: 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+  }
+}
